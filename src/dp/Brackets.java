1f3b80e9938ea68454bbbef973cc558f4aa57dc9
whitewu <whitewu@wesure.cn>
2019-03-25 10:01:27 +0800

    minor

--- src/dp/Brackets.java
+++ src/dp/Brackets.java
@@ -1,4 +1,56 @@
 package dp;
 
+/**
+ * 括号问题
+ */
 public class Brackets {
+
+
+    /**
+     * 题1：判断一个字符串是否为合法的括号
+     */
+    public static boolean isBracket1(String str) {
+        char[] cs = str.toCharArray();
+        boolean flag = true;
+        int count = 0;
+        for (char c : cs) {
+            if (c != '(' || c != ')') {
+                flag = false;
+                break;
+            }
+            if (c == ')' && --count < 0){
+                flag = false;
+                break;
+            }
+            if (c == '('){
+                count++;
+            }
+        }
+        if (count != 0) {
+            flag = false;
+        }
+        return flag;
+    }
+
+    /**
+     * 题2:判断括号中的最长合法子串
+     */
+
+    public static int getBracketSize(String str) {
+        char[] cs = str.toCharArray();
+        int[] dp = new int[str.length()];
+        int pre = 0;
+        int res = 0;
+        for (int i = 1; i < str.length(); i++) {
+            if (cs[i] == ')') {
+                pre = i - dp[i - 1] - 1;
+                if (pre >= 0 && cs[pre] == '('){
+                    dp[i] = dp[i-1]+2+(pre>0?dp[pre-1]:0);
+                }
+            }
+            res = Math.max(dp[i], res);
+        }
+        return res;
+    }
+
 }
